f6ed33737b72572b828436aa50945db6e2954c52
nam <tnam5353@gmail.com>
2023-10-07 11:17:44 +0700

    nam up

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { Outlet } from "react-router-dom";
 
 import { AuthContext } from "./hooks/useAuth";
 import ProtectedRoute from "./pages/ProtectedRoute";
-import Login from "./pages/Login";
+
 
 function App() {
-  const { userId, accountType } = useContext(AuthContext);
+
   return (
     <ProtectedRoute>
       <Outlet />
